e8f4b247977de49abd2c43725f577de8a265dadf
Peter Garnett <pete.garnett@PGARNETT-2.local>
2025-10-31 09:51:26 +0000

    feat: bootstrap sanity studio

---
+++ sanity.config.ts
@@ -0,0 +1,21 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'carhartt-resuable-page-blocks',
+
+  projectId: 'tc1rc16b',
+  dataset: 'production',
+
+  plugins: [
+    structureTool(),
+    visionTool(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
